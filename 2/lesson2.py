9a8634d198ec3167f958a4059c4e85356fbda20a
pinkfluffyunicorntaso <anastasia.kvachantiradze18@Gmail.com>
2025-01-23 00:00:47 +0400

    first commit

---
+++ 2/lesson2.py
@@ -0,0 +1,9 @@
+name = "taso"
+
+surname = "kvachantiradze"
+
+age = 12
+
+height = "165,5cm"
+
+print( "my name is " + name +" " + surname +" i am "+ str(age) +" years old and my height is "+ height )
