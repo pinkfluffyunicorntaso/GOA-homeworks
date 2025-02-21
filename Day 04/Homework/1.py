272fa68f274d6cd6e10fa80f9a9aab707697992e
pinkfluffyunicorntaso <Anastasia.kvachantiradze18@gmail.com>
2025-02-21 20:09:49 +0400

    second commit

---
+++ Day 04/Homework/1.py
@@ -0,0 +1,10 @@
+#string-ები გამოიყენება ტექსტური მონაცემების წარმოსადგენად მაგ: "hello"
+#Integer-ი გამოიყენება რიცხვული მონაცემების წარმოსადგენად მაგ: 123
+
+text = "yepiiiii"
+num = 12
+text1 = "skibidi"
+
+print(type(text))
+print(type(num))
+print(type(text1))
