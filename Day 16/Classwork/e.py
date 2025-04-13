0eefcafd5c45cc2dce887852767f44bc67e8b810
pinkfluffyunicorntaso <Anastasia.kvachantiradze18@gmail.com>
2025-04-14 00:08:39 +0400

    second commit

---
+++ Day 16/Classwork/e.py
@@ -0,0 +1,7 @@
+#თითოეულ ლოგიკურ წინადადებას მიუწერეთ რა პასუხს გამოიტას და ახსენით რატომ:
+
+#print(True and False and True or False and True and True or False or False and True) false იმიტომ, რომ and ის შემთხვევაში თუ ერთ ერთი ელემენტი არის false პასუხი იქნება false
+
+#print(False or True and True or False and True or True and False) true იმიტომ, რომ or ის შემთხვევაში თუ ერთერთი ელემენტი არის true მაშინ პასუხი იქნება trueee
+
+#print(10 > 5 and True or False and 23 == 23 and False or 100 / 100 == 1 or 5 * 5 == 30 and 10 < 30 > 20)  false იმიტომ, რომ and ის შემთხვევაში თუ ერთ ერთი ელემენტი არის false პასუხი იქნება false
